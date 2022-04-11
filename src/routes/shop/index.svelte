869fc34e255189071d1c301e2a9942d5a5dcd271
Aaron Maywood <am7274183@gmail.com>
2022-04-11 16:13:07 +0900

    挙動が不安定な Product の@container クエリーへ対策
    ページ表示時に、正しく表示される時と @container クエリーが働かないことがある。
    
    container-query-polyfill によって外部CSS の読み込みが二度起こっており、それによって引き起こされていると観察。
    外部 CSS を内部に取り込んだところ、挙動が安定した。
    
    原因は
    1. https://github.com/GoogleChromeLabs/container-query-polyfill ポリフィル用のライブラリー側
    2. SvelteKit の開発サーバー(yarn dev --open)
    
    などと考えられるが、
    
    そもそもSvelte が @container シンタックスに対応しておれば、外部CSS を書く必要も生じなかったことから、
    このやり方で根本解決したと言える。

--- src/routes/shop/index.svelte
+++ src/routes/shop/index.svelte
@@ -10,11 +10,54 @@ import PageFooter from '$lib/shop/common/PageFooter.svelte';
 	<title>The Brooklyn Garden</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
 	<script src="https://unpkg.com/container-query-polyfill/cqfill.iife.min.js"></script>
-    <link rel="stylesheet" href='/shop-product.css'>
     <style>
     body {
       font-family: system-ui;
     }
+
+	.product {
+		container: inline-size;
+	}
+
+	/* Smaller version (ie. sidebar in this demo) */
+	@container (max-width: 200px) {
+		.product-container {
+			border: 1px solid #c2ceb0;
+			padding: 0.5rem;
+			border-radius: 10px;
+			display: flex;
+			align-items: center;
+		}
+
+		.product-container h1 {
+			letter-spacing: 0;
+			width: 90px;
+			overflow: hidden;
+			text-overflow: ellipsis;
+			white-space: pre;
+			margin: 0;
+		}
+
+		.product-container span {
+			margin: 0;
+		}
+
+		.product-container p,
+		.product-container button {
+			display: none;
+		}
+	}
+
+	/* wider sizes (i.e in hero) */
+	@container (min-width: 350px) {
+		.product-container {
+			padding: 0.5rem 0 0;
+			display: flex;
+		}
+		.product button {
+			margin-left: 1rem;
+		}
+	}
     </style>
 </svelte:head>
 
