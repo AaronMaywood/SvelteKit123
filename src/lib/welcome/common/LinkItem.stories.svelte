13c8df7126a4ad2084d75536762e027e2923aa9b
Aaron Maywood <am7274183@gmail.com>
2022-04-12 17:05:58 +0900

    エッジケースを追加しました。

--- src/lib/welcome/common/LinkItem.stories.svelte
+++ src/lib/welcome/common/LinkItem.stories.svelte
@@ -14,3 +14,17 @@ import LinkItem from './LinkItem.svelte';
 		desc="How to customize webpack and Babel"
 	/>
 </Story>
+
+<Story name="エッジ:データが与えられていない時">
+	<LinkItem />
+</Story>
+
+<Story name="エッジ:タイトルや説明(desc)が長すぎる場合">
+	<LinkItem 
+		href="https://storybook.js.org/docs/react/configure/webpack"
+		src="https://raw.githubusercontent.com/storybookjs/storybook/next/examples/vue-3-cli/src/stories/assets/stackalt.svg"
+		alt="Build"
+		title="More more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more more Build configuration"
+		desc="Very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very long How to customize webpack and Babel"
+	/>
+</Story>
