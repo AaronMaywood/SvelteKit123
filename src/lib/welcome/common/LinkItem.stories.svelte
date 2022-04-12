0f4c08ec913ee7aebc3960301046610e7bb2c296
Aaron Maywood <am7274183@gmail.com>
2022-04-12 16:09:18 +0900

    初めてのストーリー定義

---
+++ src/lib/welcome/common/LinkItem.stories.svelte
@@ -0,0 +1,16 @@
+<script>
+import { Meta, Story } from '@storybook/addon-svelte-csf';
+import LinkItem from './LinkItem.svelte';
+</script>
+
+<Meta title="Welcome/LinkItem" component={LinkItem} />
+
+<Story name="標準">
+	<LinkItem 
+		href="https://storybook.js.org/docs/react/configure/webpack"
+		src="https://raw.githubusercontent.com/storybookjs/storybook/next/examples/vue-3-cli/src/stories/assets/stackalt.svg"
+		alt="Build"
+		title="Build configuration"
+		desc="How to customize webpack and Babel"
+	/>
+</Story>
