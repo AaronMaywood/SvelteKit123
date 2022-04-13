4e9a2f753313b4d96cca35c1ba3c880af19e4146
Aaron Maywood <am7274183@gmail.com>
2022-04-13 13:35:55 +0900

    共通スタイルシートをテンプレートだけ残して無効化

--- src/lib/shop/common/Product.stories.svelte
+++ src/lib/shop/common/Product.stories.svelte
@@ -26,7 +26,10 @@ const products = [
 
 <svelte:head>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+	<!--
+	.storybook/test.css
 	<link rel="stylesheet" href="./test.css">
+	-->
 	<script src="https://unpkg.com/container-query-polyfill/cqfill.iife.min.js"></script>
     <style>
 	.product {
