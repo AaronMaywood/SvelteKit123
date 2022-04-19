e43021a415a65d8cd5d130e43bbe7f131df03c34
Aaron Maywood <am7274183@gmail.com>
2022-04-19 11:22:45 +0900

    描画を更に安定させるために、await page.waitForTimeout(1000); を追加

--- tests/devices/devices.spec.js
+++ tests/devices/devices.spec.js
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('devices', async ({ page, browserName }) => {
 	await page.goto('http://localhost:3000/shop', { waitUntil: 'networkidle' });
+	await page.waitForTimeout(1000);
 	expect(await page.screenshot({fullPage:true})).toMatchSnapshot(`shop-index.png`);
 	await page.goto('http://localhost:3000/welcome');
 	expect(await page.screenshot({fullPage:true})).toMatchSnapshot(`welcome.png`);
--- tests/firstview/firstview.spec.js
+++ tests/firstview/firstview.spec.js
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('firstview', async ({ page, browserName }) => {
 	await page.goto('http://localhost:3000/shop', { waitUntil: 'networkidle' });
+	await page.waitForTimeout(1000);
 	expect(await page.screenshot()).toMatchSnapshot(`shop-index.png`);
 	await page.goto('http://localhost:3000/welcome');
 	expect(await page.screenshot()).toMatchSnapshot(`welcome.png`);
