b37d9bca503834aec5dc6877f019b3329036bbe2
Aaron Maywood <am7274183@gmail.com>
2022-04-19 11:21:40 +0900

    描画を安定させるために {waitUntil: 'networkidle'} を追加

--- tests/firstview/firstview.spec.js
+++ tests/firstview/firstview.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('firstview', async ({ page, browserName }) => {
-	await page.goto('http://localhost:3000/shop');
+	await page.goto('http://localhost:3000/shop', { waitUntil: 'networkidle' });
 	expect(await page.screenshot()).toMatchSnapshot(`shop-index.png`);
 	await page.goto('http://localhost:3000/welcome');
 	expect(await page.screenshot()).toMatchSnapshot(`welcome.png`);
--- tests/viewports/viewports.spec.js
+++ tests/viewports/viewports.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('viewports', async ({ page, browserName }) => {
-	await page.goto('http://localhost:3000/shop');
+	await page.goto('http://localhost:3000/shop', { waitUntil: 'networkidle' });
 	expect(await page.screenshot({fullPage:true})).toMatchSnapshot(`shop-index.png`);
 	await page.goto('http://localhost:3000/welcome');
 	expect(await page.screenshot({fullPage:true})).toMatchSnapshot(`welcome.png`);
