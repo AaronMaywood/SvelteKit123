32741fc9fef39faf866e9b8822077eae530d05db
Aaron Maywood <am7274183@gmail.com>
2022-05-10 14:39:39 +0900

    静的ページをstatic/test.html として配置し、そこへのリンクを貼った
    このようにhtml とコンポーネントのハイブリッド形式を利用可能
    
    ただしstatic/test.html からはコンポーネントを使うことができない。
    
    コンポーネントを使うなら、２つの方法がある。
    1. stc/routes/test.svelte のように、ページコンポーネントにする必要がある。
    2. より高度な方法として、Svelte をWeb Components 形式で書き出し、それをhtml から利用する方法もある。
        https://zenn.dev/eringiv3/articles/225e9bc2c92ff1
        ただしSvelte コンポーネントとWeb Components はコンポーネントを実現する手法として違いがある。
        違いについてはここに詳しい。
        https://zenn.dev/tnzk/articles/835d3252ce01ed
    	- Web Components はHTML タグとなり、属性も利用できる
    	- Web Components はShadow DOM を使うのでreset.css などグローバルなクラス名を持てない(Svelte のコンポーネントの方が使い勝手がいい)
    
    したがって１の方式の方が簡単確実である。

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,8 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
+<p><a href="/test.html">静的ページ(static/test.html)へのリンク</a></p>
+
 <p><a href="/welcome">Welcome to Storybook</a></p>
 <p><a href="/shop/">The Brooklyn Garden</a></p>
 
